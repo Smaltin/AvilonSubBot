ee787e0195396533164879e01dd09f88763e6f07
Smaltin <sterling.maltin@gmail.com>
2021-06-19 15:38:45 -0700

    I goofed, pretty bad.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import javax.security.auth.login.LoginException;
 
 public class Main {
     public static final long CHANNEL_ID = Long.parseLong("855909993806037003");
-    private static final String BOTKEY = "ODU1ODk3Nzc2MTI1NjQwNzA0.YM5K6w.0klOwSA3y8KES1f6llCCahoZvlw";
+    private static final String BOTKEY = System.getenv("BOTKEY");
     private static final String YOUTUBE_CHANNEL = "UCDfhh-KjgxjW_PJCcsU226g";
     private static final String YOUTUBE_API_KEY = "AIzaSyApSt7agFderSClMv0rIBWx_x-0Y1aVv8E";
     public static final String API_ENDPOINT = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + YOUTUBE_CHANNEL + "&key=" + YOUTUBE_API_KEY;
